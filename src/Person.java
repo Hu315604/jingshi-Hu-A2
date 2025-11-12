a4d537b6d62b0021662b366653a19ba1bdc8f244
Hu315604 <234374770+Hu315604@users.noreply.github.com>
2025-11-13 07:34:57 +0800

    Person class

--- src/Person.java
+++ src/Person.java
@@ -1,3 +1,43 @@
-public class Person {
+
+public abstract class Person {
+
+    private String name;
+    private String sex;
+    private int age;
+
+    public Person() {
+        // Default constructor
+    }
+
+    public Person(String name, String sex, int age) {
+        this.name = name;
+        this.sex = sex;
+        this.age = age;
+    }
+
+    // Getter and Setter
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 
 }

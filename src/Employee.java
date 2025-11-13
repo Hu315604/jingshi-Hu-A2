fc0fd5c0aec9d6ee893c24d6fa0744d96954fb17
Hu315604 <234374770+Hu315604@users.noreply.github.com>
2025-11-13 08:03:02 +0800

    Employee class

--- src/Employee.java
+++ src/Employee.java
@@ -1,3 +1,32 @@
-public class Employee {
+public class Employee extends Person{
+    private String employeeID;
+    private String position;
 
+    public Employee(){
+        // Default constructor
+    }
+
+    public Employee(String name, String sex, int age, String employeeID, String position){
+        super(name, sex, age);
+        this.employeeID = employeeID;
+        this.position = position;
+    }
+
+    public String getEmployeeID() {
+        return employeeID;
+    }
+
+    public void setEmployeeID(String employeeID) {
+        this.employeeID = employeeID;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    
 }

45768631736b231b122398ba27a2fd8495e15eaf
Hu315604 <234374770+Hu315604@users.noreply.github.com>
2025-11-13 15:22:28 +0800

    Visitor class

--- src/Visitor.java
+++ src/Visitor.java
@@ -1,3 +1,29 @@
-public class Visitor {
+public class Visitor extends Person{
 
+    private String visitorID;
+    private String phone;
+
+    public Visitor() {
+        // Default constructor
+    }
+
+    public Visitor(String name, String sex, int age, String visitorID, String phone) {
+        super(name, sex, age);
+        this.visitorID = visitorID;
+        this.phone = phone;
+    }
+
+    public String getVisitorID() {
+        return visitorID;
+    }
+    public void setVisitorID(String visitorID) {
+        this.visitorID = visitorID;
+    }
+    public String getPhone() {
+        return phone;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    
 }

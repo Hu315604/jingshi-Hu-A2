0fffe780bd4ecd5b42bc9a79a2d0af061733698a
Hu315604 <234374770+Hu315604@users.noreply.github.com>
2025-11-19 12:00:31 +0800

    finish part4A

--- src/AssignmentTwo.java
+++ src/AssignmentTwo.java
@@ -3,7 +3,9 @@ public class AssignmentTwo {
     public static void main(String[] args) {
 
         AssignmentTwo assignmentTwo = new AssignmentTwo();
-        assignmentTwo.partThree();
+        // assignmentTwo.partThree();
+        assignmentTwo.partFourA();
+
     }
 
     public void partThree() {
@@ -30,6 +32,30 @@ public class AssignmentTwo {
     }
 
     public void partFourA() {
+        Employee operator = new Employee("Jacket", "male", 51, "E002", "Ride operator");
+        Ride waterRide = new Ride("Water Ride", "Visitor with heart disease cannot ride", operator);
+        System.out.println("Create a new Ride object:"+waterRide.getName());
+
+        Visitor visitor1 = new Visitor("Zhangsan", "female", 25, "V001", "13800138001");
+        Visitor visitor2 = new Visitor("Zhangwuji", "male", 32, "V002", "13900139002");
+        Visitor visitor3 = new Visitor("Zhangsanfeng", "male", 28, "V003", "13600136003");
+        Visitor visitor4 = new Visitor("Liudehua", "male", 45, "V004", "13700137004");
+        Visitor visitor5 = new Visitor("Zhaowei", "female", 36, "V005", "13500135005");
+
+        waterRide.addVisitorToHistory(visitor1);
+        waterRide.addVisitorToHistory(visitor2);
+        waterRide.addVisitorToHistory(visitor3);
+        waterRide.addVisitorToHistory(visitor4);
+        waterRide.addVisitorToHistory(visitor5);
+
+        boolean inHisory=waterRide.checkVisitorFromHistory(visitor5);
+        if (inHisory) System.out.println(visitor5.getName()+" is in ride history.");
+
+        int num = waterRide.numberOfVisitors();
+        System.out.println("Number of visitors in "+waterRide.getName()+" ride hisroty: "+num);
+
+        waterRide.printRideHistory();
+
     }
 
     public void partFourB() {

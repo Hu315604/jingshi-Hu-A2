357bf0907b8b430e16d0362834a7f51849d0d777
Hu315604 <234374770+Hu315604@users.noreply.github.com>
2025-11-20 23:57:18 +0800

    fix comparator bug

--- src/VisitorComparator.java
+++ src/VisitorComparator.java
@@ -7,7 +7,7 @@ public class VisitorComparator implements Comparator<Visitor> {
     public int compare(Visitor v1, Visitor v2) {
 
         //use a minimum of two of the instance variables
-        int ageCompare = v1.getAge().compareTo(v2.getAge());
+        int ageCompare = Integer.compare(v1.getAge(), v2.getAge());
 
         if (ageCompare !=0) return ageCompare;
   
